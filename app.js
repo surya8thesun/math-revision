f8a5770c26db50b247a09dc8941c6fd7f5687348
Suryansh Agrawal <62498807+surya8thesun@users.noreply.github.com>
2020-09-13 14:15:59 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 // jshint esversion:6
-const startValue = document.getElementById("start").value;
-const endValue = document.getElementById("end").value;
+let nums = [];
 let objs = [];
+let p;
+let count = 0;
+let q =
+  objs[
+    Math.floor(
+      Math.random() * nums.length * document.querySelector("#range").value
+    )
+  ];
 class createTableObj {
   constructor(num1, num2) {
     this.first = num1;
@@ -9,40 +16,84 @@ class createTableObj {
     this.result = num1 * num2;
   }
 }
-let nums = [];
-let query;
-document.querySelector("form").addEventListener("submit", (event) => {
-  nums = [];
-  objs = [];
-  event.preventDefault();
-  for (
-    let i = document.getElementById("start").value;
-    i <= document.getElementById("end").value;
-    i++
-  ) {
-    nums.push(i);
+class createDomObj {
+  createShow(query) {
+    p = document.createElement("p");
+    p.innerHTML = "";
+    let arr = [];
+    objs.map((obj) => {
+      if (obj.result === query.result) {
+        arr.push({ first: obj.first, second: obj.second });
+      }
+    });
+    arr.map((thing) => {
+      p.innerHTML += `<br>`;
+      p.innerHTML = p.innerHTML + `${thing.first} ${thing.second}`;
+    });
+    arr = [];
   }
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j <= document.getElementById("range").value; j++) {
-      let obj = new createTableObj(nums[i], j);
-      objs.push(obj);
+  createCheck(query) {
+    p = document.createElement("p");
+    p.innerHTML = "";
+    p.innerHTML = query.result;
+  }
+  render(bool, q) {
+    if (bool) {
+      this.createShow(q);
+    } else {
+      this.createCheck(q);
+    }
+    document.body.append(p);
+  }
+}
+class App extends createDomObj {
+  constructor() {
+    super();
+  }
+  createLists() {
+    nums = [];
+    objs = [];
+    for (
+      let i = document.getElementById("start").value;
+      i <= document.getElementById("end").value;
+      i++
+    ) {
+      nums.push(i);
+    }
+    for (let i = 0; i < nums.length; i++) {
+      for (let j = 0; j <= document.getElementById("range").value; j++) {
+        let obj = new createTableObj(nums[i], j);
+        objs.push(obj);
+      }
+    }
+    if (count % 2 === 0) {
+      q =
+        objs[
+          Math.floor(
+            Math.random() * nums.length * document.querySelector("#range").value
+          )
+        ];
     }
   }
-  query = objs[Math.floor(Math.random() * (nums.length * 20))];
-  document.querySelector("p").textContent = `${query.result}`;
+  init(event, bool) {
+    console.log(p, this);
+    this.createLists();
+    event.preventDefault();
+    if (q) {
+      if (bool) {
+        this.render(true, q);
+      } else {
+        this.render(false, q);
+      }
+    }
+  }
+}
+const app = new App();
+document.querySelector("form").addEventListener("submit", (event) => {
+  count++;
+  app.init(event, false);
 });
 document.querySelector("button").addEventListener("click", (event) => {
-  let arr = [];
-  objs.map((obj) => {
-    if (obj.result === query.result) {
-      arr.push({ first: obj.first, second: obj.second });
-    }
-  });
-  arr.map((thing) => {
-    document.querySelector("p").innerHTML += `<br>`;
-    document.querySelector("p").innerHTML =
-      document.querySelector("p").innerHTML +
-      `${thing.first} ${thing.second}`;
-  });
-  arr = [];
+  count++;
+  app.init(event, true);
 });
